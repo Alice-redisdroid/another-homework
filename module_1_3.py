34c227d64fd928e93f4ecd97b6fac8e538842f91
Alice-redisdroid <itomoha494@gmail.com>
2024-11-18 12:27:14 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+from operator import truediv
+name = "Alice"
+print("name:",name)
+age = 18
+print("age:",age)
+new_age = 19
+age = age + new_age
+print("new_age:",age)
+is_student = True
+print("is student:",is_student)
